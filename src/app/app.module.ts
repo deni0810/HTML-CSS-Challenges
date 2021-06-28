cbee43956478fb2f5dd19528124d70c6fd39b916
deni0810 <deni.ciobanu@yahoo.com>
2021-06-28 18:38:57 +0300

    ce vrei

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LevelOneComponent } from './level-one/level-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LevelOneComponent
   ],
   imports: [
     BrowserModule,
